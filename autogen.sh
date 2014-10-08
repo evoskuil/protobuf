eef902f0102ffe2d9b6a20e2cf932035d4d5b7c1
evoskuil <eric@voskuil.org>
2014-10-07 23:42:08 -0700

    Prevent google test download and install.

--- autogen.sh
+++ autogen.sh
@@ -4,26 +4,26 @@
 # be included in the distribution.  These files are not checked in because they
 # are automatically generated.
 
-set -e
+#set -e
 
 # Check that we're being run from the right directory.
-if test ! -f src/google/protobuf/stubs/common.h; then
-  cat >&2 << __EOF__
-Could not find source code.  Make sure you are running this script from the
-root of the distribution tree.
-__EOF__
-  exit 1
-fi
+#if test ! -f src/google/protobuf/stubs/common.h; then
+#  cat >&2 << __EOF__
+#Could not find source code.  Make sure you are running this script from the
+#root of the distribution tree.
+#__EOF__
+#  exit 1
+#fi
 
 # Check that gtest is present.  Usually it is already there since the
 # directory is set up as an SVN external.
-if test ! -e gtest; then
-  echo "Google Test not present.  Fetching gtest-1.5.0 from the web..."
-  curl http://googletest.googlecode.com/files/gtest-1.5.0.tar.bz2 | tar jx
-  mv gtest-1.5.0 gtest
-fi
+#if test ! -e gtest; then
+#  echo "Google Test not present.  Fetching gtest-1.5.0 from the web..."
+#  curl http://googletest.googlecode.com/files/gtest-1.5.0.tar.bz2 | tar jx
+#  mv gtest-1.5.0 gtest
+#fi
 
-set -ex
+#set -ex
 
 # Temporary hack:  Must change C runtime library to "multi-threaded DLL",
 #   otherwise it will be set to "multi-threaded static" when MSVC upgrades
@@ -31,8 +31,8 @@ set -ex
 #   probably change their default to match, then this will be unnecessary.
 #   One of these mappings converts the debug configuration and the other
 #   converts the release configuration.  I don't know which is which.
-sed -i -e 's/RuntimeLibrary="5"/RuntimeLibrary="3"/g;
-           s/RuntimeLibrary="4"/RuntimeLibrary="2"/g;' gtest/msvc/*.vcproj
+#sed -i -e 's/RuntimeLibrary="5"/RuntimeLibrary="3"/g;
+#           s/RuntimeLibrary="4"/RuntimeLibrary="2"/g;' gtest/msvc/*.vcproj
 
 # TODO(kenton):  Remove the ",no-obsolete" part and fix the resulting warnings.
 autoreconf -f -i -Wall,no-obsolete
